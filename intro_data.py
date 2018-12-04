0b10c43e3338c055a49016e9cde944df1b7c6458
CeEmil <emil.ceraceanu@gmail.com>
2018-12-04 14:07:21 +0200

    4

--- intro_data.py
+++ intro_data.py
@@ -1,12 +1,20 @@
 import pandas as pd
 import quandl
 import math
+import numpy as np
+from sklearn import preprocessing
+from sklearn import svm
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split #cross_validation
+from sklearn.model_selection import cross_val_score
+#from sklearn import preprocessing, svm
+#cross_validation can't be found
 
 #get data from quandl
 df = quandl.get('WIKI/GOOGL')
 
 #print heads to see how the data looks like
-print(df.head())
+#print(df.head())
 
 #select data that we need
 # they are called feaures
@@ -20,7 +28,7 @@ df['HL_change'] = (df['Adj. Close'] - df['Adj. Open']) /df['Adj. Open'] * 100.0
 df = df[['Adj. Close','HL_PCT','HL_change','Adj. Volume',]]
 
 #print to see how the new model looks like
-print(df.head())
+#print(df.head())
 
 #XXXXto fill comemnt when he mentions what the fuck this is
 #what is the field that we are looking for
@@ -32,17 +40,48 @@ df.fillna(-99999, inplace=True)
 #this will be the timeframe for the forcast (nr of days)
 #0.1 is the timeline len definer (not exact days)
 #ceil - Floor and ceiling functions
-forcast_out = int(math.ceil(0.1*len(df)))
+forcast_out = int(math.ceil(0.01*len(df)))
+
+print ("forcast into the future: " + str(forcast_out))
 
 #labers are the estimations that we are trying to do
 #shift https://pandas.pydata.org/pandas-docs/stable/generated/pandas.DataFrame.shift.html
 #we are shifting estimation by forcast_out interval  forcast_out = int(math.ceil(0.1*len(df)))
+#add extra elements
 df['label'] = df[forcast_col].shift(-forcast_out)
 
 #lowering the number accuracy
 df.dropna(inplace=True)
 
-print(df.head())
+#print(df.head())
 
 #show tail of data
 #print(df.tail())
+
+#features are #
+#everything is a feature except the label
+x = np.array(df.drop(['label'],1))
+#our labels
+y = np.array(df['label'])
+#scaling the data
+#may cost processing time
+x = preprocessing.scale(x)
+y = np.array(df['label'])
+
+#creating the testing and training sets
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+#creating a classifier
+#n_jobs run multi instances; -1 run max
+clf = LinearRegression(n_jobs=-1)
+clf.fit(x_train, y_train)
+accuracy = clf.score(x_test, y_test)
+
+print("prediction accuracy for linear regression: "+ str(accuracy))
+
+
+clf = svm.SVR(gamma='auto')
+clf.fit(x_train, y_train)
+accuracy = clf.score(x_test, y_test)
+
+print("prediction accuracy for SVM SRV: "+ str(accuracy))
